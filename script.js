533ec95a578b1ca4c7635cc747be7c7ee2bfcf55
pavithrasekar24 <52492606+pavithrasekar24@users.noreply.github.com>
2023-07-24 17:30:37 +0530

    1 file modified

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ let details = [
 
 let tbody = document.getElementById('tbody');
 
-details.forEach((item) => {
+details.forEach((item, i) => {
   let row = document.createElement('tr');
   let keys = Object.keys(item);
   for (let k of keys) {
     let cell = document.createElement('td');
     cell.innerText = item[k];
-    console.log(cell);
     row.appendChild(cell);
   }
   let deleteBtn = document.createElement('button');
   deleteBtn.innerText = 'Delete';
-  deleteBtn.addEventListener('click', () => handleDelete(i));
+  deleteBtn.addEventListener('click', () => handleDelete(i, row));
 
   let deleteCell = document.createElement('td');
   deleteCell.append(deleteBtn);
@@ -26,6 +25,8 @@ details.forEach((item) => {
   tbody.appendChild(row);
 });
 
-let handleDelete = (i) => {
+let handleDelete = (i, row) => {
+  tbody.removeChild(row);
   details.splice(i, 1);
+  console.log(details);
 };
